cdca726c931023517aacbf5a0bc3ad695e2340c8
arun iOS <arun.p@doodleblue.com>
2020-10-12 12:40:09 +0530

    pod files commit

---
+++ Pods/Target Support Files/Pods-EventManageDemo-EventManageDemoUITests/Pods-EventManageDemo-EventManageDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EventManageDemo_EventManageDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EventManageDemo_EventManageDemoUITests
+@end
